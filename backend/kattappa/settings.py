1b027a4e09c1cda2908925c78b9740005a6c1165
Manikandan K <manikandan.dev.it@gmail.com>
2026-02-18 13:09:17 +0530

    13:09

--- backend/kattappa/settings.py
+++ backend/kattappa/settings.py
@@ -249,14 +249,3 @@ CSRF_COOKIE_SAMESITE = "None"
 CSRF_COOKIE_SECURE = True
 
 
-import os
-from django.contrib.auth import get_user_model
-
-User = get_user_model()
-
-if not User.objects.filter(username="admin").exists():
-    User.objects.create_superuser(
-        username="admin",
-        email="admin@gmail.com",
-        password="admin123"
-    )
